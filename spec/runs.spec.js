09001257e8ff2154379262a82b485644cfd2c639
jordanallain <jordan.allain@generalassemb.ly>
2019-01-30 10:46:07 -0500

    Use better labeling for tests
    - Use better labeling for test output.
    - Change some describe blocks to it blocks.
    - Partially addresses #58

--- spec/runs.spec.js
+++ spec/runs.spec.js
@@ -5,13 +5,13 @@ const expect = chai.expect
 
 const runs = require('../lib/runs')
 
-describe('runs', function () {
+describe('Run Tracker', function () {
   const User = runs.User
   const Run = runs.Run
   const user = new User('Christopher Robin', 'wdi@christopherRobin.com')
   user.runs.push(new Run('2015-05-25 15:00', 1200, 600))
   user.runs.push(new Run('2015-05-25 15:00', 1400, 800))
-  describe('User instance', function () {
+  describe('each User instance', function () {
     it('has a name property', function () {
       expect(Object.keys(user).includes('name')).to.equal(true)
     })
@@ -22,20 +22,14 @@ describe('runs', function () {
       expect(Object.keys(user).includes('runs')).to.equal(true)
       expect(user.runs instanceof Array).to.equal(true)
     })
-    describe('total distance', function () {
-      it('has the correct length', function () {
-        expect(user.totalDistance()).to.equal(2600)
-      })
+    it('has a totalDistance method that returns the right length', function () {
+      expect(user.totalDistance()).to.equal(2600)
     })
-    describe('longest distance run', function () {
-      it('has the correct length', function () {
-        expect(user.longestRunDistance()).to.equal(1400)
-      })
+    it('has a longestDistance method that returns the right length', function () {
+      expect(user.longestRunDistance()).to.equal(1400)
     })
-    describe('average speed', function () {
-      it('has the correct value', function () {
-        expect(user.averageSpeed().toLocaleString('en-US')).to.equal('1.857')
-      })
+    it('has an averageSpeed method that returns the correct value', function () {
+      expect(user.averageSpeed().toLocaleString('en-US')).to.equal('1.857')
     })
   })
 })
