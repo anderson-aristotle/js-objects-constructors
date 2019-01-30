422da5e9ec36a26127290a8db44a84fce645cfe3
jordanallain <jordan.allain@generalassemb.ly>
2019-01-30 10:34:37 -0500

    Add test for User instance name property
    - Add test to ensure each User instance has a 'name' property.
    - Partially addresses #58

--- spec/runs.spec.js
+++ spec/runs.spec.js
@@ -6,47 +6,29 @@ const expect = chai.expect
 const runs = require('../lib/runs')
 
 describe('runs', function () {
-  if (Object.keys(runs)[0] === 'name') {
-    describe('total distance', function () {
-      it('has the correct length', function () {
-        expect(runs.totalDistance()).to.equal(2600)
-      })
+  const User = runs.User
+  const Run = runs.Run
+  const user = new User('Christopher Robin', 'wdi@christopherRobin.com')
+  user.runs.push(new Run('2015-05-25 15:00', 1200, 600))
+  user.runs.push(new Run('2015-05-25 15:00', 1400, 800))
+  describe('User instance', function () {
+    it('has a name property', function () {
+      expect(Object.keys(user).includes('name')).to.equal(true)
     })
-
-    describe('longest distance run', function () {
-      it('has the correct length', function () {
-        expect(runs.longestRunDistance()).to.equal(1400)
-      })
-    })
-
-    describe('average speed', function () {
-      it('has the correct value', function () {
-        expect(runs.averageSpeed().toLocaleString('en-US')).to.equal('1.857')
-      })
-    })
-  } else {
-    const User = runs.User
-    const Run = runs.Run
-    const user = new User('Christopher Robin', 'wdi@christopherRobin.com')
-    user.runs.push(new Run('2015-05-25 15:00', 1200, 600))
-    user.runs.push(new Run('2015-05-25 15:00', 1400, 800))
-
     describe('total distance', function () {
       it('has the correct length', function () {
         expect(user.totalDistance()).to.equal(2600)
       })
     })
-
     describe('longest distance run', function () {
       it('has the correct length', function () {
         expect(user.longestRunDistance()).to.equal(1400)
       })
     })
-
     describe('average speed', function () {
       it('has the correct value', function () {
         expect(user.averageSpeed().toLocaleString('en-US')).to.equal('1.857')
       })
     })
-  }
+  })
 })
